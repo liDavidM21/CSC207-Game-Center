eed499d40ee539c79b292e19c7b8e4fa2716eb84
Yujie Wu <wyj1254157112@gmail.com>
2018-11-25 22:13:29 -0500

    Game switching bug fixed for slidingtiles. Previous version may switch to wrong game. Worked with Jinda Peter

--- Phase2/project/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/StartingActivity.java
+++ Phase2/project/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/StartingActivity.java
@@ -57,12 +57,11 @@ public class StartingActivity extends AppCompatActivity {
      */
     private static int showDefault = 1;
 
-    private String current_game = "Sliding Tiles";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         serializeUserManager();
-        Usermanager.get_instance().switch_game(current_game);
+        Usermanager.get_instance().switch_game("Sliding Tiles");
         super.onCreate(savedInstanceState);
         boardManager = new BoardManager();
         if (showDefault == 1) {
@@ -267,15 +266,9 @@ public class StartingActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                if (Game_choose.get_current_game().equals("sliding_tiles")) {
                     Intent tmp = new Intent(tmp1, GameActivity.class);
                     saveToFile(StartingActivity.TEMP_SAVE_FILENAME);
                     startActivity(tmp);
-                }
-                else{
-                    Intent tmp = new Intent(tmp1, MainActivity.class);
-                    startActivity(tmp);
-                }
             }
         });
     }
@@ -285,18 +278,11 @@ public class StartingActivity extends AppCompatActivity {
      */
     private void switchToSetting() {
         final StartingActivity tmp1 = this;
-        if (Game_choose.get_current_game().equals("minesweeper")) {
-            Intent tmp = new Intent(tmp1, GameSettingMinesweeper.class);
-            saveToFile(StartingActivity.TEMP_SAVE_FILENAME);
-            startActivity(tmp);
-        }
-        else {
             Intent tmp = new Intent(tmp1, GameSetting.class);
             saveToFile(StartingActivity.TEMP_SAVE_FILENAME);
             makeToastUndoText();
             startActivity(tmp);
         }
-    }
 
     /**
      * Switch to Scoreboard interface.
