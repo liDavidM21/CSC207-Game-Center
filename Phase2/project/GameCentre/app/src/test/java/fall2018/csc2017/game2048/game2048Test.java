e224e8c302de76726c4d6c2afb24c70e7c0976e9
JindaPeter <jinda.huang@mail.utoronto.ca>
2018-11-26 22:09:03 -0500

    add two packages of test

---
+++ Phase2/project/GameCentre/app/src/test/java/fall2018/csc2017/game2048/game2048Test.java
@@ -0,0 +1,28 @@
+package fall2018.csc2017.game2048;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class game2048Test {
+
+    @Test
+    public void setUndoStep() {
+    }
+
+    @Test
+    public void getUndoStep() {
+    }
+
+    @Test
+    public void getScore() {
+    }
+
+    @Test
+    public void clearScore() {
+    }
+
+    @Test
+    public void addScore() {
+    }
+}
